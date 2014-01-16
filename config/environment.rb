cfa15003bb55e9c003cf5f3de6da70c5c05a3a05
Shane Boland <sboland926@gmail.com>
2014-01-16 18:14:02 -0500

    Added Amazon S3 bucket for paperclip uploads to herkou

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Pinteresting::Application.initialize!
+
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['shane-pinteresting'],
+    :access_key_id => ENV['AKIAJSB2NXRSWNPOLCHQ'],
+    :secret_access_key => ENV['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']
+  }
+}
