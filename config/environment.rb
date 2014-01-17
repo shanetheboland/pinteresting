c1dc79ab68e16291b2c39fdcf04c42c0cddee94f
Shane Boland <sboland926@gmail.com>
2014-01-16 20:51:58 -0500

    added jquery masonry

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Pinteresting::Application.initialize!
 
-config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['AWS_BUCKET'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  }
-}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,13 @@ Pinteresting::Application.configure do
   #Required for Devise Gem
   #Set this to actual host name
   config.action_mailer.default_url_options = { :host => 'shane-pinteresting.herokuapp.com/' }
+
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
